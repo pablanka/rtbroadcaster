07e8c4628f3d5dd641ed62a95b1f2fa61daf40e4
Pablo Acuña <pacunia@interactive-3d.com>
2016-10-31 17:19:13 -0300

    State message id change

--- javascript-sdk/rtbroadcaster_sdk.js
+++ javascript-sdk/rtbroadcaster_sdk.js
@@ -1,4 +1,4 @@
-function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, errorCallback) {
+function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, errorCallback){
     var conn;
     var ref = this;
 
@@ -15,7 +15,7 @@ function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, e
         return paramValue;
     }
 
-    function sendMessageToServer(command) {
+    function sendMessageToServer(command){
         if (!conn) {
             return false;
         }
@@ -25,10 +25,10 @@ function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, e
         conn.send(command);
     }
 
-    function onConnectionOpen() {
+    function onConnectionOpen(){
         var statusValue = 2;
         var paramUUID = getUrlParam("uuid");
-        if (!paramUUID) {
+        if(!paramUUID){
             ref.isOwner = true;
             statusValue = 1;
         }
@@ -40,41 +40,41 @@ function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, e
             },
             funcKey: "",
             funcParams: [],
-            SateMessage: false,
-            StateMessageID: -1
+            SateMessage: false, 
+            StateMessageID:  ""
         }
         sendMessageToServer(JSON.stringify(message));
-        if (openCallback) { openCallback(); }
+        if(openCallback) {openCallback();}
     }
 
-    function onConnectionClose(evt) {
+    function onConnectionClose(evt){
         console.log("Connection closed.");
-        if (closeCallback) { closeCallback(); }
+        if(closeCallback){ closeCallback();}
     }
 
-    function onReceiveConnectionMessage(evt) {
+    function onReceiveConnectionMessage(evt){
         var messages = evt.data.split('\n');
         for (var i = 0; i < messages.length; i++) {
             var message = messages[i];
             objMessage = JSON.parse(message);
-            if (objMessage.FuncKey !== "CameraRot") {
+            if(objMessage.FuncKey !== "CameraRot"){
                 console.log("MESSAGE: ", objMessage);
             }
-            if (objMessage.UUID) {
+            if(objMessage.UUID){
                 ref.uuid = objMessage.UUID;
-                switch (objMessage.Status.Value) {
+                switch(objMessage.Status.Value){
                     case 0:
                         //
                         break;
                     case 3:
-                        if (!ref.connected) {
+                        if(!ref.connected){
                             ref.connected = true;
-                            if (ref.isOwner) {
+                            if(ref.isOwner){
                                 console.log("ROOM UUID: ", objMessage.UUID);
                             }
-                            if (onConnectionCallback) { onConnectionCallback(objMessage.UUID); }
+                            if(onConnectionCallback) {onConnectionCallback(objMessage.UUID);}
                         }
-                        if (objMessage.FuncKey) {
+                        if(objMessage.FuncKey){
                             var _func = ref.suscribedFuncs.get(objMessage.FuncKey);
                             _func(objMessage.FuncParams);
                         }
@@ -88,32 +88,32 @@ function RTBroadcaster(url, onConnectionCallback, openCallback, closeCallback, e
     }
 
     if (window["WebSocket"]) {
-        conn = new WebSocket(url); // "ws://localhost:8080/broadcasting"
+        conn = new WebSocket(url);
         conn.onopen = onConnectionOpen;
         conn.onclose = onConnectionClose;
         conn.onmessage = onReceiveConnectionMessage;
     } else {
         var error = "Your browser does not support WebSockets.";
         console.error(error);
-        if (errorCallback) { errorCallback(error); }
+        if(errorCallback) {errorCallback(error);}
     }
 }
 
 // API
-RTBroadcaster.prototype.suscribeFunc = function (key, _func) {
+RTBroadcaster.prototype.suscribeFunc = function(key, _func){
     this.suscribedFuncs.set(key, _func);
 }
 
-RTBroadcaster.prototype.unsuscribeFunc = function (key) {
+RTBroadcaster.prototype.unsuscribeFunc = function(key){
     this.suscribedFuncs.delete(key);
 }
 
-RTBroadcaster.prototype.sendAction = function (key, params, itsStateMessage, stateID) {
-    if (!itsStateMessage) {
+RTBroadcaster.prototype.sendAction = function(key, params, itsStateMessage){
+    if(!itsStateMessage){
         itsStateMessage = false;
     }
     var uuid = this.uuid;
-    var message = {
+     var message = {
         UUID: uuid,
         Status: {
             Value: 3, // Connetion status: 0 = not connected, 1 = new, 2 = join, 3 = connected, 4 = closed
@@ -121,8 +121,8 @@ RTBroadcaster.prototype.sendAction = function (key, params, itsStateMessage, sta
         },
         FuncKey: key,
         FuncParams: params,
-        SateMessage: itsStateMessage,
-        StateMessageID: (stateID ? stateID : -1)
+        SateMessage: itsStateMessage, 
+        StateMessageID: (itsStateMessage ? key : "")
     }
     this.sendMessage(JSON.stringify(message));
 }
